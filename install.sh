7630a4604d6bcef0ca27fe2a352ce186ed00bed3
Brian Henning <henninb@msn.com>
2026-03-04 16:15:48 -0600

    wip

--- install.sh
+++ install.sh
@@ -19,6 +19,11 @@ if [ "$(uname)" = "Linux" ]; then
   curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.appimage
   chmod u+x nvim-linux-x86_64.appimage
   sudo mv nvim-linux-x86_64.appimage /usr/local/bin/nvim
+
+  sudo apt install -y ripgrep
+  sudo apt install -y starship
+  sudo apt install -y fish
+  sudo npm install -g tree-sitter-cli
 fi
 
 echo ""
